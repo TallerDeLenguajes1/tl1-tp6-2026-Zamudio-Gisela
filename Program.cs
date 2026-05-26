cc7eb31b239f424868c08e38cc0ad65787bdc0d5
Zamudio-Gisela <zagarogi@gmail.com>
2026-05-26 02:21:44 -0300

    Punto 2 terminado

--- Program.cs
+++ Program.cs
@@ -1,53 +1,144 @@
 ﻿using System.ComponentModel;
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-int a;
+// int a;
 
-int b;
+// int b;
 
-a = 10;
+// a = 10;
 
-b = a;
+// b = a;
 
-Console.WriteLine("Valor de a: " + a);
+// Console.WriteLine("Valor de a: " + a);
 
-Console.WriteLine("Valor de b: " + b);
+// Console.WriteLine("Valor de b: " + b);
 
-Console.WriteLine("Ingrese un numero mayor a 0 para invertirlo: ");
+// Console.WriteLine("Ingrese un numero mayor a 0 para invertirlo: ");
 
-int numero;
-bool esInt;
+// int numero;
+// bool esInt;
+
+// do
+// {
+//     esInt = int.TryParse(Console.ReadLine(), out numero);
+
+//     if (!esInt || numero <= 0)
+//     {
+//         if (!esInt)
+//         {
+//             Console.WriteLine("EL valor ingresado NO es un numero");
+//         }
+
+//         if (esInt && numero <= 0)
+//         {
+//             Console.WriteLine("El numero ingresado no es mayor a 0");
+//         }
+
+//         Console.WriteLine("Vuelva a ingresar el numero: ");
+//     }
+
+// } while (!esInt || numero <= 0);
+
+// int resto;
+// int invertido = 0;
+
+// while (numero != 0)
+// {
+//     resto = numero % 10;
+//     invertido = invertido * 10 + resto;
+//     numero = numero / 10;
+// }
+
+// Console.WriteLine($"El invertido del numemro ingresado es: {invertido}");
+
+
+int opcion, repetir = 0;
+float num1, num2, resultado;
+bool esopInt, esnum1Int, esnum2Int, esrepInt;
 
 do
 {
-    esInt = int.TryParse(Console.ReadLine(), out numero);
+    Console.WriteLine("************************  CALCULADORA  ****************************");
 
-    if (!esInt || numero <= 0)
+    do
     {
-        if (!esInt)
+        Console.WriteLine("Ingrese el numero que coresponda a la operacion que desea realizar");
+        Console.WriteLine("         SUMAR ---------------->   1");
+        Console.WriteLine("         RESTAR --------------->   2");
+        Console.WriteLine("         MULTIPLICAR ---------->   3");
+        Console.WriteLine("         DIVIDIR -------------->   4");
+
+        esopInt = int.TryParse(Console.ReadLine(), out opcion);
+
+        if (!esopInt)
         {
-            Console.WriteLine("EL valor ingresado NO es un numero");
+            Console.WriteLine("El valor ingresado no es un numero");
         }
 
-        if (esInt && numero <= 0)
-        {
-            Console.WriteLine("El numero ingresado no es mayor a 0");
-        }
+    } while (!esopInt || (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4));
 
-        Console.WriteLine("Vuelva a ingresar el numero: ");
+    do
+    {
+        Console.WriteLine("Ingrese en primer numero: ");
+        esnum1Int = float.TryParse(Console.ReadLine(), out num1);
+
+        if (!esnum1Int)
+        {
+            Console.WriteLine("El valor ingresado no es un numero");
+        }
+    } while (!esnum1Int);
+
+    do
+    {
+        Console.WriteLine("Ingrese el segundo numero: ");
+        esnum2Int = float.TryParse(Console.ReadLine(), out num2);
+
+        if (!esnum2Int)
+        {
+            Console.WriteLine("El valor ingresado no es un numero");
+        }
+    } while (!esnum2Int);
+
+
+    switch (opcion)
+    {
+        case 1:
+            resultado = num1 + num2;
+            Console.WriteLine($"El resultado de la SUMA es: {resultado}");
+            break;
+        case 2:
+            resultado = num1 - num2;
+            Console.WriteLine($"El resultado de la RESTA es: {resultado}");
+            break;
+        case 3:
+            resultado = num1 * num2;
+            Console.WriteLine($"El resultado de la MULTIPLICACION es: {resultado}");
+            break;
+        case 4:
+            resultado = num1 / num2;
+            Console.WriteLine($"El resultado de la DIVISION es: {resultado}");
+            break;
+        default:
+            break;
     }
 
-} while (!esInt || numero <= 0);
+    do
+    {
+        Console.WriteLine("Desea realizar otro calculo?: ");
+        Console.WriteLine("  'SI', presione ----->  1");
+        Console.WriteLine("  'NO', presione ----->  0");
+        esrepInt = int.TryParse(Console.ReadLine(), out repetir);
 
-int resto;
-int invertido = 0;
+        if (!esrepInt)
+        {
+            Console.WriteLine("El valor ingresado no es un numero");
+        }
+    } while (!esrepInt || (repetir != 1 && repetir != 0));
 
-while (numero != 0)
-{
-    resto = numero % 10;
-    invertido = invertido * 10 + resto;
-    numero = numero / 10;
-}
+    if (repetir == 0)
+    {
+        Console.WriteLine("************************  FIN  ****************************");
+    }
+} while (repetir == 1);
 
-Console.WriteLine($"El invertido del numemro ingresado es: {invertido}");
